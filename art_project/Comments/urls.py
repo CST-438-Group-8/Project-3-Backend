acbd8e700e4a51ba3de364c891b3e03e9f6bdd6b
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2024-11-12 11:12:54 -0800

    update for comments
    comments Endpoints visual now works

--- art_project/Comments/urls.py
+++ art_project/Comments/urls.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 # from django.urls import include, path
 from rest_framework.urls import path
+from django.urls import path, include
+# from .views import *
 # from .views import getComments, deleteComment, editComment, addComment 
-from .views import getComments, addComment, editComment, deleteComment
+
+from .views import getComments, addComment, editComment, deleteComment, CommentsViewSet
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'Comments', CommentsViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('comments/', getComments, name='Get All Comments'),
     path('delComment', deleteComment, name = 'delete Comment'),
     path('addComment', addComment, name = 'Add comment'),
     path('editComment', editComment, name= 'edit Comment'),
 
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
