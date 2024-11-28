e40b525386f5a1fc593082b100821b81f906445b
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2024-11-27 17:49:37 -0800

    potential fix

--- art_project/Comments/serializer.py
+++ art_project/Comments/serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Comments
 
 class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user_id.username', read_only=True)
+    # added this line up above
     class Meta:
         model = Comments
         fields = ['comment_id', 'comment', 'user_id', 'post_id']
